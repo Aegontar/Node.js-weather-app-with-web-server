5688e96b2e08d89045172b741a0ff1872bb93dd2
aegontar <charlesdessa1@hotmail.com>
2020-06-04 05:37:23 +0600

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
-  
+
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()
 
@@ -15,7 +15,7 @@ weatherForm.addEventListener('submit', (e) => {
   const location = search.value
 
 
-  fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+  fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
       if (data.error) {
         console.log(data.error)
@@ -32,11 +32,11 @@ weatherForm.addEventListener('submit', (e) => {
   })
 
 
- 
 
 
 
- 
+  (/e/Development/Back-end/node.js/Udemy course/Web-server/.ssh/id_rsa)
+
 
   console.log(location)
 }) 
